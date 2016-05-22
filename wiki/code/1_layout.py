26b432b6406d4c4befd7706dcc87fee67c9bd59a
Richard Jarvis <rjarvis@rwba.org.uk>
2016-05-22 12:11:36 +0100

    Files for the wiki

---
+++ wiki/code/1_layout.py
@@ -0,0 +1,9 @@
+from rwbatools import gui  
+def changeLabel(btn):  
+      app.setLabel("l2", app.getEntry("text"))  
+app = gui()  
+app.addLabel("l1", "Simple Demo")
+app.addEntry("text")
+app.addButton("OK", changeLabel)
+app.addEmptyLabel("l2")
+app.go()
