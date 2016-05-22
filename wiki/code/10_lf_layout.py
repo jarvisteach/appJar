2d49fa00f86bb26a2a3730e7f399787fef73b7b7
Richard Jarvis <rjarvis@rwba.org.uk>
2016-05-22 12:53:27 +0100

    wiki stuff

---
+++ wiki/code/10_lf_layout.py
@@ -0,0 +1,21 @@
+from rwbatools import gui
+
+def music(btn):
+      if btn == "Stop":
+            app.stopSound()
+      elif btn == "Play":
+            song.app.getRadioButton("song")+".wav"
+            app.playSound(song)
+
+app=gui()
+
+app.startLabelFrame("Songs")
+app.addRadioButton("song", "Killer Queen")
+app.addRadioButton("song", "We Will Rock You")
+app.addRadioButton("song", "Don't Stop Me Now")
+app.addRadioButton("song", "We Are The Champions")
+app.stopLabelFrame()
+
+app.addButtons(["Play","Stop"], music)
+
+app.go()
