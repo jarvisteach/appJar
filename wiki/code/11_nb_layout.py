155503acd54817d1e22fa9a00edf4a7e7173ae14
Richard Jarvis <rjarvis@rwba.org.uk>
2016-05-22 13:00:14 +0100

    wiki stuff

---
+++ wiki/code/11_nb_layout.py
@@ -0,0 +1,16 @@
+from rwbatools import gui
+
+app=gui("NoteBook Demo", "200x200")
+
+app.startNoteBook("nb")
+app.startNoteTab("One")
+app.addLabel("l1", "Tab One")
+app.stopNoteTab()
+app.startNoteTab("Two")
+app.addLabel("l2", "Tab Two")
+app.stopNoteTab()
+app.startNoteTab("Three")
+app.addLabel("l3", "Tab Three")
+app.stopNoteTab()
+app.stopNoteBook()
+app.go()
