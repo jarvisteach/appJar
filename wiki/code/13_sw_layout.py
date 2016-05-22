2c265ce9e5a90926da65157db457027d8dc31b8b
Richard Jarvis <rjarvis@rwba.org.uk>
2016-05-22 17:53:28 +0100

    wiki stuff

---
+++ wiki/code/13_sw_layout.py
@@ -0,0 +1,16 @@
+from rwbatools import gui
+
+def show(btn): app.showSubWindow("sub")
+def hide(btn): app.hideSubWindow("sub")
+
+app=gui()
+app.addLabel("l1", "Sub Window Demo")
+app.addButton("PRESS", show)
+
+app.startSubWindow("sub")
+app.addLabel("s1", "sub")
+app.addButton("Stop", hide)
+app.stopSubWindow()
+app.setSubWindowLocation("sub", 400,400)
+
+app.go()
