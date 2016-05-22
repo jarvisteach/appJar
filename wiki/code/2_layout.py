e63d6e6d4df037247b45429b8e9d26c4ee67db4c
Richard Jarvis <rjarvis@rwba.org.uk>
2016-05-22 12:27:15 +0100

    Wiki stuff

---
+++ wiki/code/2_layout.py
@@ -0,0 +1,25 @@
+from rwbatools import gui
+
+def press(btn):
+      if btn=="Save":
+            n=app.getEntry("name")
+            a=app.getEntry("age")
+            r=app.getOptionBox("role")
+            app.infoBox("Details", "You entered: " + n + ", " + str(a) + ", " + r)
+      elif btn=="Quit":
+            app.stop()
+
+
+app=gui()
+
+app.addLabel("l1", "Name", 0, 0)
+app.addLabel("l2", "Age", 1, 0)
+app.addLabel("l3", "Role", 2, 0)
+
+app.addEntry("name", 0, 1)
+app.addNumericEntry("age", 1, 1)
+app.addOptionBox("role", ["Teacher", "Student", "Developer", "Volunteer"],2, 1)
+
+app.addButtons(["Save", "Quit"], press, 3, 0, 2)
+
+app.go()
