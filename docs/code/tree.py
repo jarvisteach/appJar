ff959c3c268b243bc96e396d1366b2e210d91717
Richard Jarvis <rjarvis@rwba.org.uk>
2016-06-03 13:01:21 +0100

    Tree Test code update

--- docs/code/tree.py
+++ docs/code/tree.py
@@ -1,6 +1,9 @@
 from rwbatools import gui
-
-app=gui()
+def dClick(btn):
+    print("dclick:",btn)
+def press(btn):
+    print("press:", app.getTree("t1"))
+app=gui("Tree", "400x400")
 app.addTree("t1",
             """<people>
             <person><name>Fred</name><age>45</age><gender>Male</gender></person>
@@ -8,4 +11,6 @@ app.addTree("t1",
             <person><name>CLive</name><age>28</age><gender>Male</gender></person>
             <person><name>Betty</name><age>51</age><gender>Female</gender></person>
             </people>""")
+app.addTreeFunction("t1", dClick)
+app.addButton("PRESS", press)
 app.go()
