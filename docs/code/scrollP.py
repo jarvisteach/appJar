af3350b855d5b8c717847ab189019fbadcaa6eb4
Richard Jarvis <rjarvis@rwba.org.uk>
2016-06-04 06:07:55 +0100

    mkdocs - multi test

--- docs/code/scrollP.py
+++ docs/code/scrollP.py
@@ -1,11 +1,17 @@
 from rwbatools import gui
 cols=["red","blue"]
 
-app=gui()
+app=gui("Scroll Pane", "300x200")
 app.startScrollPane("sp")
 for i in range(150):
     idd="L"+str(i)
-    app.addLabel(idd, "In the pane")
+    app.addLabel(idd, "In the paneeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
+    app.setLabelBg(idd, cols[i%2])
+app.stopScrollPane()
+app.startScrollPane("sp2")
+for i in range(150):
+    idd="LL"+str(i)
+    app.addLabel(idd, "In the paneeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
     app.setLabelBg(idd, cols[i%2])
 app.stopScrollPane()
 app.addLabel("l2", "Out scroll pane")
