73118a497cc5da143ac1d71d246f555979323162
Richard Jarvis <rjarvis@rwba.org.uk>
2016-06-04 05:28:09 +0100

    mkdocs

--- docs/code/listB.py
+++ docs/code/listB.py
@@ -1,15 +1,17 @@
 from rwbatools import gui
 
 def press(btn):
-    app.removeListItem("l1", app.getListItems("l1")[0])
+    app.removeListItemAtPos("l1", app.getListItemsPos("l1")[0])
 
 def add(btn):
-    app.addListItem("l1", [app.getEntry("e1")])
+    app.addListItem("l1", app.getEntry("e1"))
 
-items=["dog", "cat", "kangaroo", "elephant", "zebra", "crocodile", "pig", "lion", "tiger", "jaguar", "puma"]
+items=["dog", "cat", "kangaroo", "elephant", "dog", "zebra", "crocodile", "pig", "lion", "tiger", "jaguar", "puma", "dog"]
 
 app=gui()
 app.addListBox("l1", items)
+app.setListBoxRows("l1",15)
+app.setListSingle("l1", False)
 app.addButton("DEL", press)
 app.addEntry("e1")
 app.addButton("ADD", add)
--- docs/code/scrollP.py
+++ docs/code/scrollP.py
@@ -1,9 +1,12 @@
 from rwbatools import gui
+cols=["red","blue"]
 
 app=gui()
 app.startScrollPane("sp")
-for i in range(50):
-    app.addLabel("L"+str(i), "In the pane")
+for i in range(150):
+    idd="L"+str(i)
+    app.addLabel(idd, "In the pane")
+    app.setLabelBg(idd, cols[i%2])
 app.stopScrollPane()
 app.addLabel("l2", "Out scroll pane")
 app.go()
