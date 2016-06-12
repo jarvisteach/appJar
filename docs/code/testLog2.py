ca0a4e06a4bc9173f64e05e193f547ae035bdfbc
Richard Jarvis <rjarvis@rwba.org.uk>
2016-06-12 08:14:18 +0100

    appJar rebranding

--- docs/code/testLog2.py
+++ docs/code/testLog2.py
@@ -6,8 +6,11 @@ def press(btn):
         app.stop()
     else:
         print("User:", app.getEntry('user'), "Pass:", app.getEntry('pass'))
+        app.setStatus(app.getEntry("user"))
 
-app = gui()
+app = gui("Login Window", "400x200")
+app.setBg("brown")
+app.setFont(20)
 
 app.addLabel("title", "Welcome to appJar", 0, 0, 2)     # Row 0, Column 0, Span 2
 app.addLabel("user", "Username:", 1, 0)                 # Row 1, Column 0, no span
@@ -17,5 +20,8 @@ app.addSecretEntry("pass", 2, 1)                        # Row 2, Column 1, no sp
 app.addButtons(["Submit", "Cancel"], press, 3, 0, 2)    # Row 3, Column 0, Span 2
 
 app.setEntryFocus("user")
+#app.addStatus()
+#app.setStatusBg("green")
+#app.setStatusFg("blue")
 
 app.go()
