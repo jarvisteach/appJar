d6869284c2d3b8e11b9d01221b0f46de79186abd
Richard Jarvis <rjarvis@rwba.org.uk>
2016-07-30 00:20:39 +0900

    Introduced a grip widget...

--- docs/code/prettyGrid.py
+++ docs/code/prettyGrid.py
@@ -13,6 +13,7 @@ app.addLabel("4", "4", 1, 2)
 app.addLabel("5", "5", 0, 3, 0, 2) # span 2 rows
 app.addLabel("6", "6", 2)
 app.addLabel("7", "7", 2, 2, 2) # span 3 cols
+app.addGrip(5, 0, 3)
 # set some colours
 app.setLabelBg("1", "red")
 app.setLabelBg("2", "blue")
