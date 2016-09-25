270949991f52d504132fbb91f01fb54ff886e5bf
Richard Jarvis <rjarvis@rwba.org.uk>
2016-09-25 20:40:16 +0100

    fixed test progs

--- docs/code/1_tree.py
+++ docs/code/1_tree.py
@@ -3,5 +3,5 @@ def press(btn): print(btn)
 app=gui()
 data="<people><person><name>Richard</name><age>21</age></person><person><name>kh</name><age>44</age></person></people>"
 app.addTree("tree", data)
-app.addTreeFunction("tree", press)
+app.setTreeDoubleClickFunction("tree", press)
 app.go()
