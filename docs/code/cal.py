2fa03790448ccc30598a45a666221a9a4236cccc
Richard Jarvis <rjarvis@rwba.org.uk>
2016-09-28 22:21:20 +0100

    Simple example of a calendar
    Will eventually move into appJar as a new widget

--- docs/code/cal.py
+++ docs/code/cal.py
@@ -1,29 +1,56 @@
 from appJar import gui
 import calendar
+import datetime
 
-def setDays(btn):
-    month = months.index(app.getOptionBox("Month")) + 1
+STARTDATE = 1920
+ENDDATE = 2020
+MONTH_NAMES = calendar.month_name[1:]
+
+def updateDays(btn=None):
+    day = app.getOptionBox("Day")
+    month = MONTH_NAMES.index(app.getOptionBox("Month")) + 1
     year = int(app.getOptionBox("Year"))
     days = range(1, calendar.monthrange(year, month)[1]+1)
-
     app.changeOptionBox("Day", days)
+    # keep previous date possible
+    try: app.setOptionBox("Day", day)
+    except: pass
+
+def getDate():
+    day = int(app.getOptionBox("Day"))
+    month = MONTH_NAMES.index(app.getOptionBox("Month")) + 1
+    year = int(app.getOptionBox("Year"))
+    date = datetime.date(year, month, day)
+    return date
+
+def setDate(date):
+    app.setOptionBox("Day", date.day-1)
+    app.setOptionBox("Month", date.month-1)
+    app.setOptionBox("Year", str(date.year))
+
+def showDate(btn):
+    print(getDate())
 
 
 
-startDate = 1920
-endDate = 2020
 
-days = []
-months = calendar.month_name[1:]
-years=range(startDate, endDate)
+def buildWidgets():
+    days = []
+    years=range(STARTDATE, ENDDATE)
+
+    app.addLabel("dl", "Day:", 0, 0)
+    app.addOptionBox("Day", days, 0, 1)
+    app.addLabel("ml", "Month:", 1, 0)
+    app.addOptionBox("Month", MONTH_NAMES,1,1)
+    app.addLabel("yl", "Year:", 2, 0)
+    app.addOptionBox("Year", years,2,1)
+    app.setOptionBoxFunction("Month", updateDays)
+    app.setOptionBoxFunction("Year", updateDays)
+    app.addButton("GET", showDate, 3, 0, 2)
+
+    updateDays()
 
 app=gui()
-
-app.addLabelOptionBox("Day", days)
-app.addLabelOptionBox("Month", months)
-app.addLabelOptionBox("Year", years)
-app.setOptionBoxFunction("Month", setDays)
-app.setOptionBoxFunction("Year", setDays)
-
-
+buildWidgets()
+setDate(datetime.date.today())
 app.go()
