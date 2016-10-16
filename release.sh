4d1cb7b62b2323f18b7ed7c38761e85ff5cfbcc4
Richard Jarvis <rjarvis@rwba.org.uk>
2016-10-16 14:05:08 +0100

    Latest release

--- release.sh
+++ release.sh
@@ -2,4 +2,4 @@
 # 
 # Release script
 # 
-zip -r releases/appJar.zip __init__.py appjar.py gpl-3.0.txt lib resources
+zip -r releases/appJar.zip __init__.py appjar.py showcase.py gpl-3.0.txt lib resources
