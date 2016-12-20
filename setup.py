3fd367973e600f76d102910a8f310d92be31e256
jarvisteach <jarvisteach@gmail.com>
2016-12-20 15:42:11 +0000

    rearranging for PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name="appJar",
+    package=["appJar"],
+    version="0.1",
+    description="A GUI wrapper for tkinter",
+    author="Richard Jarvis",
+    author_email="info@appjar.info",
+    url="https://github.com/jarvisteach/appJar",
+    download_url="http://github.com/jarvisteach/appJar/tarball/0.1",
+    keywords=["python", "gui", "tkinter"],
+    classifiers=[],
+)
