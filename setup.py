af2eec16146c09064ffeec456e0d0da1d803d322
jarvisteach <jarvisteach@gmail.com>
2016-12-20 22:18:23 +0000

    Setup for PyPI

--- setup.py
+++ setup.py
@@ -2,16 +2,14 @@ from distutils.core import setup
 setup(
     name="appJar",
     packages=["appJar"],
-    version="0.19",
+    version="0.02",
     description="A GUI wrapper for tkinter",
     author="Richard Jarvis",
     author_email="info@appjar.info",
-    url="https://github.com/jarvisteach/appJar",
-    download_url="http://github.com/jarvisteach/appJar/tarball/0.19",
+    url="http://appJar.info",
     keywords=["python", "gui", "tkinter"],
     classifiers=[],
-    include_package_date = True,
     package_data = {
-        "appJar": ["lib/*", "resources/icons/*"]
+        "appJar": ["lib/*", "resources/icons/*", "examples/*"]
     }
 )
