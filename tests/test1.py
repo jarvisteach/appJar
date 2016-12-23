4fcf525ae53e1d097ede4069758ebc42370545c7
Richard Jarvis <jarvisteach@gmail.com>
2016-12-23 19:00:41 +0000

    New travis tests

--- tests/test1.py
+++ tests/test1.py
@@ -21,6 +21,7 @@ app=gui()
 
 
 def test_labels():
+    print("Testing labels")
     app.addEmptyLabel("el1")
     app.addLabel("l1", TEXT_ONE)
     app.addFlashLabel("fl1", TEXT_ONE)
@@ -44,6 +45,7 @@ def test_labels():
     assert app.getLabel("el1") == EMPTY
     assert app.getLabel("l1") == EMPTY
     assert app.getLabel("fl1") == EMPTY
+    print(" >> all tests complete")
 
 def test_entries():
     app.addEntry("e1")
@@ -160,11 +162,148 @@ def test_checks():
     assert app.getCheckBox(TEXT_TWO) == True
     assert app.getCheckBox(TEXT_THREE) == False
 
+def test_options():
+    pass
+
+def test_spins():
+    pass
+
+def test_lists():
+    pass
+
+def test_scales():
+    app.addScale("s1")
+    app.addScale("s2")
+    app.addScale("s3")
+    app.addScale("s4")
+
+    assert app.getScale("s1") == 0
+    assert app.getScale("s2") == 0
+    assert app.getScale("s3") == 0
+    assert app.getScale("s4") == 0
+
+    app.setScale("s1", 20)
+    app.setScale("s2", 73)
+    app.setScale("s3", 100)
+    app.setScale("s4", 101)
+
+    assert app.getScale("s1") == 20
+    assert app.getScale("s2") == 73
+    assert app.getScale("s3") == 100
+    assert app.getScale("s4") == 100
+
+    app.setScalaRange("s1", 44, 88)
+    app.setScaleRange("s2", 22, 55, 33)
+#    print(app.getScale("s1"))
+#    assert app.getScale("s1") == 44
+    assert app.getScale("s2") == 33
+
+    app.setScaleIntervals("s3", 5)
+    app.setScaleValue("s4", 101)
+    assert app.getScale("s3") == 100
+    assert app.getScale("s4") == 100
+
+
+def test_messages():
+    app.addMessage("m1", TEXT_ONE)
+    app.addMessage("m2", TEXT_TWO)
+    app.addEmptyMessage("m3")
+    app.addEmptyMessage("m4")
+
+    assert app.getMessageWidget("m1").cget("text") == TEXT_ONE
+    assert app.getMessageWidget("m2").cget("text") == TEXT_TWO
+    assert app.getMessageWidget("m3").cget("text") == EMPTY
+    assert app.getMessageWidget("m4").cget("text") == EMPTY
+
+    app.setMessage("m1", EMPTY)
+    app.setMessage("m2", TEXT_ONE)
+    app.setMessage("m3", TEXT_THREE)
+    app.setMessage("m4", EMPTY)
+
+    assert app.getMessageWidget("m1").cget("text") == EMPTY
+    assert app.getMessageWidget("m2").cget("text") == TEXT_ONE
+    assert app.getMessageWidget("m3").cget("text") == TEXT_THREE
+    assert app.getMessageWidget("m4").cget("text") == EMPTY
+
+    app.clearMessage("m2")
+    app.clearMessage("m3")
+
+    assert app.getMessageWidget("m1").cget("text") == EMPTY
+    assert app.getMessageWidget("m2").cget("text") == EMPTY
+    assert app.getMessageWidget("m3").cget("text") == EMPTY
+    assert app.getMessageWidget("m4").cget("text") == EMPTY
+
+def test_text_areas():
+    app.addTextArea("t1")
+    app.addTextArea("t2")
+    app.addScrolledTextArea("st1")
+    app.addScrolledTextArea("st2")
+
+    assert app.getTextArea("t1") == EMPTY
+    assert app.getTextArea("t2") == EMPTY
+    assert app.getTextArea("st1") == EMPTY
+    assert app.getTextArea("st2") == EMPTY
+
+    app.setTextArea("t1", TEXT_ONE)
+    app.setTextArea("t2", TEXT_TWO)
+    app.setTextArea("st1", TEXT_THREE)
+    app.setTextArea("st2", TEXT_FOUR)
+
+    assert app.getTextArea("t1") == TEXT_ONE
+    assert app.getTextArea("t2") == TEXT_TWO
+    assert app.getTextArea("st1") == TEXT_THREE
+    assert app.getTextArea("st2") == TEXT_FOUR
+
+    app.clearTextArea("t2")
+    app.clearTextArea("st1")
+
+    assert app.getTextArea("t1") == TEXT_ONE
+    assert app.getTextArea("t2") == EMPTY
+    assert app.getTextArea("st1") == EMPTY
+    assert app.getTextArea("st2") == TEXT_FOUR
+
+def test_meters():
+    app.addMeter("m1")
+#    print(app.getMeter("m1"))
+    assert app.getMeter("m1")[0] == 0
+    app.setMeter("m1", 45)
+#    print(app.getMeter("m1"))
+    assert app.getMeter("m1")[0] == 0.45
+
+def test_properties():
+    pass
+
+def test_separators():
+    app.addHorizontalSeparator()
+    app.addVerticalSeparator()
+
+def test_links():
+    app.addLink("l1", None)
+    app.addWebLink("l1", "http://appJar.info")
+
+def test_grips():
+    app.addGrip()
+    app.addGrip()
+
+def test_date_pickers():
+    pass
 
 test_labels()
 test_entries()
 test_buttons()
 test_radios()
 test_checks()
+test_options()
+test_spins()
+test_lists()
+test_scales()
+test_messages()
+test_text_areas()
+test_meters()
+test_properties()
+test_separators()
+test_links()
+test_grips()
+test_date_pickers()
 
 test_sets()
