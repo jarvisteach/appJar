12f9830c128bcd64f574fcba4fabd7eaacaeecda
Richard Jarvis <jarvisteach@gmail.com>
2016-12-23 17:32:14 +0000

    Travis - latest tests

--- tests/test1.py
+++ tests/test1.py
@@ -5,6 +5,9 @@ EMPTY=""
 
 TEXT_ONE = "l_one_x"
 TEXT_TWO = "l_one_y"
+TEXT_THREE = "l_one_z"
+TEXT_FOUR = "l_one_a"
+TEXT_FIVE = "l_one_b"
 
 NUM_ONE = 23123
 NUM_TWO = 33221
@@ -90,6 +93,21 @@ def test_entries():
     assert app.getEntry("se1") == EMPTY
     assert app.getEntry("ae1") == EMPTY
 
+def test_buttons():
+    app.addButton("b1", None)
+    app.addButtons(["bb1", "bb2", "bb3", "bb4"], None)
+    app.addButtons([["a2b1", "a2b2", "a2b3", "a2b4"], ["b2b1", "b2b2", "b2b3", "b2b4"], ["c2b1", "c2b2", "c2b3", "c2b4"]], None)
+    app.addNamedButton("butName", "nb1", None) # name/title
+
+    but1 = app.getButtonWidget("b1")
+    but2 = app.getButtonWidget("bb1")
+    but3 = app.getButtonWidget("a2b1")
+#    but4 = app.getButtonWidget("nb1")
+    assert but1.cget("text") == "b1"
+    assert but2.cget("text") == "bb1"
+    assert but3.cget("text") == "a2b1"
+#    assert but4.cget("text") == "butName"
+
 def test_sets():
     app.setLabelBg("l1", COL_ONE)
     app.setLabelFg("l1", COL_TWO)
@@ -109,7 +127,44 @@ def test_sets():
     assert lab.cget("relief") == "sunken"
     assert lab.cget("state") == "disabled"
 
+def test_radios():
+    app.addRadioButton("rb", TEXT_ONE)
+    app.addRadioButton("rb", TEXT_TWO)
+    app.addRadioButton("rb", TEXT_THREE)
+
+    assert app.getRadioButton("rb") == TEXT_ONE
+
+    app.setRadioButton("rb", TEXT_TWO)
+    assert app.getRadioButton("rb") == TEXT_TWO
+
+    app.setRadioTick("rb")
+    assert app.getRadioButton("rb") == TEXT_TWO
+
+    app.setRadioButton("rb", TEXT_THREE)
+    assert app.getRadioButton("rb") == TEXT_THREE
+
+def test_checks():
+    app.addCheckBox(TEXT_ONE)
+    app.addCheckBox(TEXT_TWO)
+    app.addCheckBox(TEXT_THREE)
+
+    assert app.getCheckBox(TEXT_ONE) == False
+    assert app.getCheckBox(TEXT_TWO) == False
+    assert app.getCheckBox(TEXT_THREE) == False
+
+    app.setCheckBox(TEXT_ONE)
+    app.setCheckBox(TEXT_TWO, True)
+    app.setCheckBox(TEXT_THREE, False)
+
+    assert app.getCheckBox(TEXT_ONE) == True
+    assert app.getCheckBox(TEXT_TWO) == True
+    assert app.getCheckBox(TEXT_THREE) == False
+
 
 test_labels()
 test_entries()
+test_buttons()
+test_radios()
+test_checks()
+
 test_sets()
