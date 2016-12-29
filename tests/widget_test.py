df7773d95e97b5b8cff2b5e34f9bc180f86a99e8
Richard Jarvis <jarvisteach@gmail.com>
2016-12-29 04:32:44 +0000

    updated menu tests

--- tests/widget_test.py
+++ tests/widget_test.py
@@ -710,43 +710,48 @@ def test_menus():
     app.addMenuItem("MEN2", "MM2", tester_function, shortcut="k", underline=2)
     app.addMenuSeparator("MEN2")
     app.addMenuCheckBox("MEN2", "CB2", tester_function, shortcut="c", underline=2)
-    app.addMenuRadioButton("MEN2", "BB2", tester_function, shortcut="r", underline=2)
-    app.addMenuRadioButton("MEN2", "BB3", tester_function)
+    app.addMenuRadioButton("MEN2", "a", "BB2", tester_function, shortcut="r", underline=2)
+    app.addMenuRadioButton("MEN2", "a", "BB3", tester_function)
     app.addSubMenu("MEN2", "sub1")
     app.addMenuItem("sub1", "MMM2", tester_function, shortcut="w", underline=2)
     app.addMenuSeparator("sub1")
     app.addMenuCheckBox("sub1", "CB23", tester_function, shortcut="x", underline=2)
-    app.addMenuRadioButton("sub1", "BB23", tester_function, shortcut="y", underline=2)
-    app.addMenuRadioButton("sub1", "BB33", tester_function, underline=0)
+    app.addMenuRadioButton("sub1", "b", "BB23", tester_function, shortcut="y", underline=2)
+    app.addMenuRadioButton("sub1", "b", "BB33", tester_function, underline=0)
     app.addMenu("PRESS", tester_function, "P", 4)
 
     assert app.getMenuCheckBox("MEN2", "CB2") is False
     assert app.getMenuCheckBox("sub1", "CB23") is False
 
-    print( app.getMenuRadioButton("MEN2", "BB2") )
-#    assert app.getMenuRadioButton("MEN2", "BB2") is False
-#    assert app.getMenuRadioButton("MEN2", "BB3") is True
-#    assert app.getMenuRadioButton("sub1", "BB23") is True
-#    assert app.getMenuRadioButton("sub1", "BB33") is False
+    assert app.getMenuRadioButton("MEN2", "a") == "BB2"
+    assert app.getMenuRadioButton("sub1", "b") == "BB23"
 
     app.setMenuCheckBox("MEN2", "CB2")
 
-    app.setMenuRadioButton("MEN2", "BB3", True)
+    app.setMenuRadioButton("MEN2", "a", "BB3")
 
     assert app.getMenuCheckBox("MEN2", "CB2") is True
     assert app.getMenuCheckBox("sub1", "CB23") is False
 
-#    assert app.getMenuRadioButton("MEN2", "BB2") is False
-#    assert app.getMenuRadioButton("MEN2", "BB3") is True
-#    assert app.getMenuRadioButton("sub1", "BB23") is True
-#    assert app.getMenuRadioButton("sub1", "BB33") is False
+    assert app.getMenuRadioButton("MEN2", "a") == "BB3"
+    assert app.getMenuRadioButton("sub1", "b") == "BB23"
 
-    app.setMenuRadioButton("sub1", "BB33", True)
+    app.setMenuRadioButton("sub1", "b", "BB33")
 
-#    assert app.getMenuRadioButton("MEN2", "BB2") is False
-#    assert app.getMenuRadioButton("MEN2", "BB3") is True
-#    assert app.getMenuRadioButton("sub1", "BB23") is False
-#    assert app.getMenuRadioButton("sub1", "BB33") is True
+    assert app.getMenuRadioButton("MEN2", "a") == "BB3"
+    assert app.getMenuRadioButton("sub1", "b") == "BB33"
+
+    app.setMenuCheckBox("MEN2", "CB2", True)
+    app.setMenuCheckBox("sub1", "CB23", True)
+
+    assert app.getMenuCheckBox("MEN2", "CB2") is True
+    assert app.getMenuCheckBox("sub1", "CB23") is True
+
+    app.setMenuCheckBox("MEN2", "CB2", True)
+    app.setMenuCheckBox("sub1", "CB23", False)
+
+    assert app.getMenuCheckBox("MEN2", "CB2") is True
+    assert app.getMenuCheckBox("sub1", "CB23") is False
 
     app.addMenuPreferences(tester_function)
     app.addMenuWindow()
@@ -759,8 +764,9 @@ def test_menus():
     app.addEntry("RCLICK")
     app.addMenuEdit()
 
-    app.disableMenuBar()
-    app.enableMenuBar()
+    app.enableMenubar()
+    app.disableMenubar()
+    app.enableMenubar()
 
     app.enableMenu("MEN2")
     app.disableMenu("MEN2")
