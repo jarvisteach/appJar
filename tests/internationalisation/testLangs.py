8350ffd2c646ddc5a562348d27bebfcbd9a902eb
jarvisteach <jarvisteach@gmail.com>
2017-01-21 10:42:20 +0000

    moved internationalisation to tests

---
+++ tests/internationalisation/testLangs.py
@@ -0,0 +1,44 @@
+import sys
+
+sys.path.append("../../../")
+from appJar import gui
+
+def press(btn):
+    app.changeLanguage(btn)
+app=gui()
+app.showSplash()
+
+app.addLabel("l1", "default text")
+app.addButtons(["English", "Korean", "French"], press)
+app.addLabel("l2", "default text")
+app.addLabel("l3", "default text")
+app.addLabelEntry("Genome")
+app.addLabelScale("s1")
+app.addMessage("m1", "Default message text")
+
+app.addListBox("fruits", ["apples", "oranges", "tomatoes"])
+app.addOptionBox("fruits", ["apples", "oranges", "tomatoes"])
+app.addSpinBox("fruits", ["apples", "oranges", "tomatoes"])
+
+app.addCheckBox("b1")
+app.addCheckBox("b2")
+app.addCheckBox("b3")
+
+app.addRadioButton("name", "b1")
+app.addRadioButton("name", "b2")
+app.addRadioButton("name", "b3")
+app.addRadioButton("name", "b4")
+
+app.addRadioButton("age", "b1")
+app.addRadioButton("age", "b2")
+app.addRadioButton("age", "b3")
+
+app.addLink("l1", None)
+app.addWebLink("l2", "http://www.appJar.info")
+
+app.addMeter("m1")
+app.addEntry("e1")
+app.addEntry("e2")
+app.setEntryDefault("e1", "<DEFAULT>")
+
+app.go(language="ENGLISH")
