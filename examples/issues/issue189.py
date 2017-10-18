193b98d2566de1a35333408475d460c35bd2c689
Richard Jarvis <jarvisteach@gmail.com>
2017-10-18 21:36:39 +0100

    updated ttk testing #189

--- examples/issues/issue189.py
+++ examples/issues/issue189.py
@@ -4,12 +4,18 @@ sys.path.append("../../")
 from appJar import gui
 app = gui("ttk demo")
 with app.labelFrame("LabelFrame"):
-    app.addLabel("l1", "Simple Label")
-    app.addCheckBox("Tick me")
-    app.addRadioButton("tb", "Tick me")
-    app.addTextArea("t1")
-    app.addButton("Press Me", None)
-    app.addScale("Scale")
-    app.addEntry("Entry")
+    with app.notebook("nb"):
+        with app.note("note1"):
+            app.addLabel("l1", "Simple Label")
+            app.addCheckBox("Tick me")
+            app.addRadioButton("tb", "Tick me")
+            app.addTextArea("t1")
+            app.addButton("Press Me", None)
+            app.addScale("Scale")
+            app.addEntry("Entry")
+            app.addButtons(["a", "b", "c"], None)
+            app.addLabelEntry("fill me")
+            app.addLabels(["a", "b", "c"])
+        with app.note("note2"):
+            pass
 app.go()
-
