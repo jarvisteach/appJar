17388d11d944f95f99ea9c30a06ace57ad5d77d6
Richard Jarvis <jarvisteach@gmail.com>
2017-11-07 01:23:33 +0000

    Testing issue #288

---
+++ examples/issues/issue288.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append("../../")
+
+def press(btn):
+    if btn == "SUB":
+        app.showSubWindow("Sub")
+        app.hide()
+    if btn == "POPUP":
+        app.infoBox("INFO", "INFO")
+
+from appJar import gui
+
+with gui("Main Window", startWindow="Sub") as app:
+    app.label("title", "Main Window")
+    app.button("SUB", press)
+
+    with app.subWindow("Sub"):
+        app.label("sub", "SubWindow")
+        app.button("POPUP", press)
+
