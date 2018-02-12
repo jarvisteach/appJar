64a4e522e8231806ee48f60aeca07ed644787a02
Richard Jarvis <jarvisteach@gmail.com>
2018-02-12 14:52:03 +0000

    Playing with colours #373

--- examples/issues/issue373.py
+++ examples/issues/issue373.py
@@ -3,8 +3,8 @@ sys.path.append("../../")
 
 def changeColour():
     app.label("DEMO", bg=app.colourBox())
-    app.label("DEMO", l.cget("bg"))
+    app.label("DEMO", app.getLabelWidget("DEMO").cget("bg"))
 
 from appJar import gui
 with gui("Colours", "400x400", sticky="news") as app:
-    l = app.label("DEMO", submit=changeColour)
+    app.label("DEMO", submit=changeColour)
