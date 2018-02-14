a554149ec8c12f2ca1dfcec3e9beb4ff20de9186
Richard Jarvis <jarvisteach@gmail.com>
2018-02-14 08:13:19 +0000

    testing issue #363

---
+++ examples/issues/issue363.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.append("../../")
+from appJar import gui
+
+rescan = '\u2753 Scan again'
+connect = '\u260D Connect'
+frame = 'Manual Connection'
+address = 'Address'
+tcp = 'TCP'
+timeout = 'Timeout'
+info = '\u2139 Just an (IP) address is required, leave the rest blank to use the defaults.'
+
+with gui() as app:
+    with app.labelFrame('{}'.format(frame), 0, 0, 2):
+        # Entry.
+        app.entry('addr', pos=(0, 1), sticky='nesw')
+        app.entry('tcp', kind='numeric', pos=(1, 1))
+        app.entry('timeout', kind='numeric', pos=(3, 1))
+        app.setFocus('addr')
+        app.setEntryMaxLength('tcp', 5)
+        app.setEntryMaxLength('timeout', 3)
+        app.setEntryDefault('addr', '{}'.format(address))
+        app.setEntryDefault('tcp', '{}'.format(tcp))
+        app.setEntryDefault('timeout', '{}'.format(timeout))
+
