57d18176a0d8899389c43af96db9bc86e24657b3
Richard Jarvis <jarvisteach@gmail.com>
2018-03-01 19:31:42 +0000

    Testing issue #394

---
+++ examples/issues/issue394.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append("../../")
+from appJar import gui
+
+def select_all(e):
+    try: e.widget.tag_add("sel","1.0","end")
+    except:
+        try: e.widget.select_range(0, 'end') 
+        except: gui.trace('Unable to select all')
+    print(app.topLevel.clipboard_get())
+
+with gui("timer", font={'size':20}) as app:
+    ta = app.textArea("text")
+    ta.config(relief='groove', bd=3)
+    e = app.entry("entry")
+
+    ta.bind('<Command-a>', select_all)
+    e.bind('<Command-a>', select_all)
