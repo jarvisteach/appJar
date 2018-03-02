70765fb859ffb4683ce5f302fcdfe4bf09905a54
Richard Jarvis <jarvisteach@gmail.com>
2018-03-02 16:29:35 +0000

    Investigating #235

--- examples/issues/issue235_ent.py
+++ examples/issues/issue235_ent.py
@@ -12,6 +12,9 @@ def submit(btn):
 def drop(btn):
     print(btn, "drop")
 
+def over(btn):
+    print(btn, "over")
+
 def press(btn):
     app.entry("e2", bg="green")
     
@@ -21,12 +24,12 @@ with gui("Entry Demo") as app:
     app.entry("e1", "focus", focus=True, bg="red", fg="green", justify="right", borderwidth=5)
     app.entry("e2", "ch-su-drop", change=change, submit=submit, drop=True)
     app.entry("e3", "over-drag", over=change, drag=change, limit=5, case="upper")
-    app.entry("e4", "tooltip-menu", tooltip="hello", menu=True)
+    app.entry("e4", "tooltip-menu", tooltip="hello", right=True)
     app.entry("e5", kind="numeric")
     app.entry("e7", ["a", "aa", "aab", "b", "c"], kind="auto")
-    app.entry("e8", kind="file", change=change, over=change)
+    app.entry("e8", kind="file", change=change, over=over)
     app.entry("e9", kind="directory", change=change)
-    app.label("l1", "hiya", focus=True, menu=True)
+    app.label("l1", "hiya", focus=True, right=True)
     x = app.gr()
     app.text("t1", pos=(x, 0), change=change, drop=drop, fg="red")
     app.text("t2", scroll=True, pos=(x, 1), over=change, bg="pink")
