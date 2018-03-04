c72a69191e25225677cd14d789aa69bae819ef11
Richard Jarvis <jarvisteach@gmail.com>
2018-03-04 16:30:38 +0000

    Updated #401

--- examples/issues/issue401.py
+++ examples/issues/issue401.py
@@ -1,7 +1,10 @@
 import sys
 sys.path.append("../../")
-
 from appJar import gui
+
+def details():
+    app.label("DETAILS", app.getTreeSelected("t1"))
+    app.label("DETAILS2", app.getTreeSelectedXML("t1"))
 
 with gui("appJar Explorer") as app:
     app.addTree("t1",
@@ -24,4 +27,7 @@ with gui("appJar Explorer") as app:
             </tabbedFrame>
         </topLevel>""")
 
-    app.label("Details Here", row=0, column=1)
+    with app.frame("DETAILS", row=0, column=1):
+        app.label("DETAILS")
+        app.label("DETAILS2")
+    app.button("DETAILS", details, colspan=2)
