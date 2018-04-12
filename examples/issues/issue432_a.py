87fb56590b9f92ba0312e1d003cec2796d094e0d
jarvisteach <jarvisteach@gmail.com>
2018-04-12 23:09:54 +0100

    investigating #432

--- examples/issues/issue432_a.py
+++ examples/issues/issue432_a.py
@@ -3,14 +3,9 @@ sys.path.append("../../")
 
 from appJar import gui
 
-def press(btn):
-    if btn == "ONE": app.selectFrame("stack", 0)
-    elif btn == "TWO": app.selectFrame("stack", 1)
-
 with gui() as app:
-    with app.frameStack("stack"):
-        with app.frame():
-            app.label("In frame one")
-        with app.frame():
-            app.label("In frame two")
-    app.buttons(["ONE", "TWO"], press)
+    with app.frame("ONE", 0,0):
+        app.label("In frame one")
+    with app.frame("TWO", 0,0):
+        app.label("In frame two")
+    app.buttons(["ONE", "TWO"], app.raiseFrame)
