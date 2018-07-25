acdaddd110576e68042d26ce5da3c89ca579fd09
Richard Jarvis <jarvisteach@gmail.com>
2018-07-25 11:23:19 +0100

    Investigating #492

---
+++ examples/issues/issue492.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append("../../")
+
+from appJar import gui
+
+def refillFrame():
+    with app.frame('one'):
+        app.removeAllWidgets(True)
+    app.queueFunction(fillFrame)
+
+def fillFrame():
+    with app.frame('one'):
+        for x in 'abcdefghijklmnopqrstuvwxyz':
+            app.label('hello'+x, bg=app.RANDOM_COLOUR())
+
+with gui() as app:
+    with app.tabbedFrame('tabs'):
+        with app.tab('one'):
+            fillFrame()
+        with app.tab('two'):
+            app.button('redo', refillFrame)
