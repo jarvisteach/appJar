d04eaa18fe2feca18721276a1dce00cb862eb0c6
Richard Jarvis <jarvisteach@gmail.com>
2018-07-30 00:19:40 +0100

    Investigating #495

---
+++ examples/issues/issue495.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append("../../")
+
+from appJar import gui
+data = [["Name", "Age", "Gender"], ["Fred", 45, "Male"],
+        ["Tina", 37, "Female"], ["Clive", 28, "Male"],
+        ["Betty", 51, "Female"]]
+
+def func():
+    print(app.getTableLastChange('g1'))
+
+with gui() as app:
+    app.table("g1", data, showMenu=True, edit=func)
