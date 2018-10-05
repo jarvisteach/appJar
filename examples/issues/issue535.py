ef9c79c4b828ea6a147b446643d434f34054f637
Richard Jarvis <jarvisteach@gmail.com>
2018-10-06 00:37:29 +0100

    Investigating #535

--- examples/issues/issue535.py
+++ examples/issues/issue535.py
@@ -6,7 +6,7 @@ from numpy import sin, pi, arange
 import random, time
 
 def getXY():
-    x = arange(0.0, 3.0, 0.01)
+    x = arange(0.0, app.slider('yVal')+1, 0.01)
     y = sin(random.randint(1,app.slider('yVal') +1) * pi * x)
     return x,y 
 
@@ -16,9 +16,10 @@ def generate():
         time.sleep(1)
 
 with gui() as app:
-    app.label('MatPlotLib Demo')
-    app.slider('yVal', show=True, pos=[2,0])
-    app.addPlot("p1", *getXY(), row=1)
+    app.label('MatPlotLib Demo', colspan=2)
+    app.slider('yVal', show=True, pos=[1,1], sticky='ns', direction='vertical')
+    app.slider('xVal', show=True, pos=[2,0], colspan=2, sticky='ew')
+    app.plot("p1", *getXY(), row=1)
 
     app.thread(generate)
 
