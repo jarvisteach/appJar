9906075c4b008b8905fc01fa99bdc03ba876e41d
Richard Jarvis <jarvisteach@gmail.com>
2019-10-19 08:14:42 +0100

    Investigating issue #484

--- examples/issues/issue484.py
+++ examples/issues/issue484.py
@@ -3,15 +3,10 @@ sys.path.append("../../")
 
 from appJar import gui
 
-tabs=['NW', 'NE', 'SW', 'SE']
-
-with gui('TABS', '400x400') as app:
-    with app.tabbedFrame('tabs'):
-        for tab in tabs:
-            with app.tab(tab, stretch='both'):
-                app.config(sticky='news', stretch='both', bg=app.RANDOM_COLOUR())
-                with app.frame(tab):
-    #                app.setSticky(tab)
-                    app.setStretch('both')
-                    for l in range(1,5):
-                        app.label(tab+'demo'*l, bg=app.RANDOM_COLOUR(), fg=app.RANDOM_COLOUR(), stretch='none', sticky=tab)
+with gui() as app:
+    app.label('hello world')
+    with app.labelFrame('demo'):
+        app.stretch='none'
+        app.label('this one')
+        app.label('this two')
+        app.label('this three')
