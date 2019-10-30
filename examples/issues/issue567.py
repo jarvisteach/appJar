e837645aa5a5c2f8b230ee6d2de75559d0abd468
Richard Jarvis <jarvisteach@gmail.com>
2019-10-30 11:14:32 +0000

    Hiding API keys from git #567 & #566

--- examples/issues/issue567.py
+++ examples/issues/issue567.py
@@ -1,8 +1,11 @@
-import sys
+import imp, sys
+keys = imp.load_source('keys', 'issue567.keys')
 sys.path.append("../../")
 
 from appJar import gui
 
 with gui() as app:
     app.label('hello world')
-    app.map('a')
+    app.map('a', mapKey=keys.MAP_KEY)
+    app.setGoogleMapKey('a', keys.MAP_KEY)
+    app.setGoogleMapLocationKey('a', ipinfo=keys.IP_INFO)
